35bd9c3aca63df1916c8118f3d96b4624b1ab2de
Chen PoTa <double1217@gmail.com>
2017-08-18 17:39:48 +0800

    Introduce MSMQ

---
+++ msmq-reader/msmq-reader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Messaging;
+
+namespace msmq_reader
+{
+    class Program
+    {
+        private const String QueueName = @".\Private$\my-test-queue";
+
+        static void Main(string[] args)
+        {
+            MessageQueue myQueue;
+            if (MessageQueue.Exists(QueueName))
+                myQueue = new MessageQueue(QueueName);
+            else
+                myQueue = MessageQueue.Create(QueueName, false);
+
+            myQueue.Formatter = new XmlMessageFormatter(
+                new Type[]
+                {
+                    typeof(String)
+                });
+
+            Message myMsg = myQueue.Receive();
+
+            Console.WriteLine((String)myMsg.Body);
+
+            myQueue.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ msmq-writer/msmq-writer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Messaging;
+
+namespace msmq_writer
+{
+    class Program
+    {
+        //private const String QueueName = @".\Private$\my-test-queue";
+        private const String QueueName = @"FormatName:DIRECT=TCP:192.168.56.102\Private$\my-test-queue";
+        private const String MyMsg = "test message...";
+
+        static void Main(string[] args)
+        {
+            MessageQueue myQueue = new MessageQueue(QueueName);
+
+            /*if (myQueue.Transactional == true)
+            {
+                MessageQueueTransaction myTransaction = new  MessageQueueTransaction();
+
+                myTransaction.Begin();
+
+                myQueue.Send(MyMsg, myTransaction);
+
+                myTransaction.Commit();
+            }
+            else*/
+            {
+                myQueue.Send(MyMsg);
+            }
+
+            myQueue.Close();
+        }
+    }
+}
